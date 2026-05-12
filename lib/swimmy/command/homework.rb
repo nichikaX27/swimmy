ef07170b568e28f5d2531f8c83393e489bd6c68f
nichikaX27 <pochinichi@gmail.com>
2026-05-12 11:21:45 +0900

    Refactor homework command to change keyword handling to title and update response messages

--- lib/swimmy/command/homework.rb
+++ lib/swimmy/command/homework.rb
@@ -1,17 +1,16 @@
 module Swimmy
   module Command
     class Homework < Swimmy::Command::Base
-
       command "homework" do |client, data, match|
         cli_dir = "/home/abo-n/git/RASK_CLI_TEMPLATE"
         cli_path = "#{cli_dir}/target/debug/rask-cli"
         rask_url = "https://rask.nomlab.org"
 
-        keywords = match[:expression] || ""
+        title = match[:expression] || ""
 
         begin
           result = Dir.chdir(cli_dir) do
-            `#{cli_path} get --documents --keywords "#{keywords}" 2>&1`
+            `#{cli_path} get --documents --title "#{title}" 2>&1`
           end
         rescue => e
           client.say(channel: data.channel, text: "コマンドの実行中にエラーが発生しました: #{e.message}")
@@ -45,13 +44,13 @@ module Swimmy
         end
 
         if names.empty?
-          message = "文書(#{keywords})中に宿題はありません．"
+          message = "文書(#{title})中に宿題はありません．"
         else
           # 担当者名・AI番号・doc_idをペアで表示
-          message = "文書(#{keywords})中の宿題担当(敬称略)\n " 
+          message = "文書(#{title})中の宿題担当を表示します．\n 以下のリンクからタスクを作成してください．\n" 
           client.say(channel: data.channel, text: message)
           names.zip(ai_numbers, doc_ids, task_urls).each do |name, ai_num, doc_id, task_url|
-            client.say(channel: data.channel, text: "<#{task_url}|#{name}{AI#{ai_num}}}>")
+            client.say(channel: data.channel, text: "<#{task_url}|#{name}{#{ai_num}}}>")
           end
         end
 
